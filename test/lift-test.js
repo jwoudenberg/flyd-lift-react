bc4426cac8c6b1aead2ccd6e5d7deb00651e87ed
Jasper Woudenberg <mail@jasperwoudenberg.com>
2016-09-03 10:24:16 +0200

    Fix tests using new version of Jest

--- test/lift-test.js
+++ test/lift-test.js
@@ -4,7 +4,6 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const TestUtils = require('react-addons-test-utils');
 const stream = require('flyd').stream;
-jest.dontMock('../');
 const Lift = require('../');
 
 const TestComponent = (props) => (
